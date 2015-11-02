c59750b6ba61b2bd3f7ae4631bc85b72b9bc6bc4
adityap <i91987@SLCITS-L2232.hcinsight.net>
2015-11-02 08:13:21 -0700

    intial web api doc gen using swagger custom UI

---
+++ WebApiDocGenSwagger/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiDocGenSwagger.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        /// <summary>
+        /// This is the first Get documentation
+        /// </summary>
+        /// <returns>Enumerable string</returns>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// This is the first Get documentation
+        /// </summary>
+        /// <returns>Enumerable string</returns>
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
